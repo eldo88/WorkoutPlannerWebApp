38517cd92b8bc02bf0cd2b6ae9f2e88a17f1df17
eldo88 <leegjones88@gmail.com>
2023-09-18 13:56:56 -0600

    Deleted un-needed import from UserService

--- src/main/java/com/workoutplanner/exercise/user/UserService.java
+++ src/main/java/com/workoutplanner/exercise/user/UserService.java
@@ -1,7 +1,6 @@
 package com.workoutplanner.exercise.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
