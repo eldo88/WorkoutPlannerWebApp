7b2d8909028ad8acbc4ce55002a97febc52127c4
eldo88 <leegjones88@gmail.com>
2023-10-28 12:36:41 -0600

    Adding folders for upcoming chznges

--- src/main/java/com/workoutplanner/exercise/user/UserController.java
+++ src/main/java/com/workoutplanner/exercise/user/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.data.repository.query.Param;
